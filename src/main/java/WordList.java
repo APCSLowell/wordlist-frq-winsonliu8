a145fd6655b9c21aa4aec029e985f36119900a96
winsonliu8 <146293819+winsonliu8@users.noreply.github.com>
2025-04-07 23:27:07 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -13,7 +13,7 @@ public class WordList
     /* to be implemented in part (a) */
     int sum = 0; 
     for (int i = 0; i < myList.size(); i++){
-      if (myList.get(i).length == len){
+      if (myList.get(i).length() == len){
           sum++; 
       }
     }
@@ -24,7 +24,7 @@ public class WordList
   {
     /* to be implemented in part (b) */
     for (int i = 0; i < myList.size(); i++){
-      if (myList.get(i).length == len){
+      if (myList.get(i).length() == len){
         myList.remove(i); 
         i--; 
       }
